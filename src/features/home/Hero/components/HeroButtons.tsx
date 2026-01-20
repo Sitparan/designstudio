7536e31cc0f68a05365d83b6f87a632ee997e1dd
Sitparan <sitpu06@gmail.com>
2026-01-20 10:48:33 -0800

    update

--- src/features/home/Hero/components/HeroButtons.tsx
+++ src/features/home/Hero/components/HeroButtons.tsx
@@ -14,12 +14,13 @@ export function HeroButtons() {
       transition={{ duration: 0.5, delay: 0.6 }}
       className="flex flex-col sm:flex-row gap-4"
     >
-       <Link href={`/work`}>
+       
       <button className="border border-white px-8 py-3 text-sm uppercase tracking-widest hover:bg-white hover:text-black transition-colors flex items-center justify-center">
-        {heroConfig.buttons.primary.label}   <ArrowRight className="ml-2 h-4 w-4" />
-      
+        <Link href={`/work`}>
+        {heroConfig.buttons.primary.label} 
+        </Link>
       </button>
-      </Link>
+
       <button className="border border-white px-8 py-3 text-sm uppercase tracking-widest  hover:bg-white hover:text-black transition-colors">
         <Link href={`/about`}>{heroConfig.buttons.secondary.label}</Link>
       </button>
